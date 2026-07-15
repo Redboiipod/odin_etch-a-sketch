2703ba373ba7210e749782b3b69bec380bd4ad74
Joseph <142068099+Redboiipod@users.noreply.github.com>
2026-07-15 16:43:11 -0700

    Create prompt and color hovering effect
    Prompt asks user to specify grid size, while hovering over squares now randomly changes its color.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,48 @@
 const container = document.querySelector(".container");
 
-
-
+function getRandomRgb() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
 function create(size){
     
+    
 
-
-    let totalSquares = (size * size);
-  
-    // let sizing = 50;
-
-    let sizing = ((1000/totalSquares) * size);   /* (1000/size) also works, oh well */
-    alert(sizing);
-
-    for ( let i = 0; i < totalSquares; i++) {      
-    const squares = document.createElement("div");
-        squares.classList.add("squares");
-        squares.style.width = `${sizing}px`;
-        squares.style.height = `${sizing}px`;
-        container.appendChild(squares);    
-
+    for ( let i = 0; i < (size * size); i++) {      
+    let squares = document.createElement("div");
+            squares.classList.add("squares");
+            squares.style.width = `${100 / size}%`;
+            squares.style.height = `${100 / size}%`;
+            squares.addEventListener("mouseenter", (e) => {
+                e.target.style.backgroundColor = getRandomRgb();
+            });
+            
+            container.appendChild(squares);  
     }
 }
 
-create(4);
+
+create(16);
+
+const buttonPrompt = document.querySelector("button");
+    buttonPrompt.addEventListener("click", () => {
+     
+        let userInput = prompt("Choose a grid size between 0 and 100");
+
+            if ((userInput > 100) || (userInput <= 0)){
+                alert("Please choose a number less than 100");
+            }
+            else {
+            const innerDivs = container.querySelectorAll("div");
+                innerDivs.forEach(div => div.remove());
+            create(userInput);
+            }
+      
+    });
+    
+
+
+
 
